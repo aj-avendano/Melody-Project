bf3b9d6b66f956e9cbbb83e199d6ce2757d0cf88
KuldeepThiara <kuldeepsingh998@gmail.com>
2022-11-02 03:00:46 -0700

    Updated the Dashboard and homepage function. Also added comments

--- Melody/dash_app/views.py
+++ Melody/dash_app/views.py
@@ -7,11 +7,15 @@ from django.contrib.auth.forms import AuthenticationForm
 
 ##Function Written by Jason Eissayou
 def dashboard(request):
-	return render(request, template_name='dash_app/dashboard.html')
-
-def homepage(request):
 	return render(request=request, template_name='dash_app/dashboard.html')
 
+#Function created Kuldeep 
+def homepage(request):
+	#takes you to he home page 
+	return render(request=request, template_name='dash_app/home.html')
+
+#Function created Kuldeep 
+#Function for register 
 def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
@@ -19,11 +23,15 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
+			#If signup is successful the you will be redirected to dashboard page. 
 			return redirect("dash_app:dashboard")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
+	#If login is unsuccessful the you will be redirected to register page. 
 	return render (request=request, template_name="dash_app/register.html", context={"register_form":form})
 
+#Function created Kuldeep 
+#Function for login
 def login_request(request):
 	
 	if request.method == "POST":	
@@ -36,15 +44,20 @@ def login_request(request):
 			if user is not None:
 				login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
+				#If login is successful the you will be redirected to dashboard page. 
 				return redirect ("dash_app:dashboard")
 			else:
 				messages.error(request,"Invalid username or password.")
 		else:
 			messages.error(request,"Invalid username or password.")
 	form = AuthenticationForm()
+	#If login is unsuccessful the you will be redirected to login  page. 
 	return render(request=request, template_name="dash_app/login.html", context={"login_form":form})
-
+	#Function for logout
+	
+#Function created Kuldeep 
 def logout_request(request):
 	logout(request)
 	messages.info(request, "You have successfully logged out.") 
+	#redirected to login page. 
 	return redirect("dash_app:login")
