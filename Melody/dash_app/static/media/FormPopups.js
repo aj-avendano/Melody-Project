07c95ea89d1a11d48286509d5596170e29188955
Eissayou <eissayouj@gmail.com>
2022-11-09 21:13:03 -0800

    Dashboard Popup Update

---
+++ Melody/dash_app/static/media/FormPopups.js
@@ -0,0 +1,16 @@
+//Made by Jason 11/9/22
+function openPreferencesForm() {
+    document.getElementById("myPreferencesForm").style.display = "block";
+  }
+
+  function closePreferencesForm() {
+    document.getElementById("myPreferencesForm").style.display = "none";
+  }
+
+  function openGenerateForm() {
+    document.getElementById("myGenerateForm").style.display = "block";
+  }
+
+  function closeGenerateForm() {
+    document.getElementById("myGenerateForm").style.display = "none";
+  }
