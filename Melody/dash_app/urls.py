2db2c35e0c3b26a2dbe3b531feced1c24bb88213
KuldeepThiara <kuldeepsingh998@gmail.com>
2022-12-06 21:17:58 -0800

    Changed homepage path to .as_view class based

--- Melody/dash_app/urls.py
+++ Melody/dash_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 app_name = "dash_app"
 
 ##Function Written by Jason Eissayou and Kuldeep and Joaquin
@@ -11,15 +12,16 @@ urlpatterns = [
     path('login', views.LoginRequest.as_view(), name='login'),
     path("logout", views.logout_request, name= "logout"),
     #path('dashboard', views.dashboard, name="dashboard"),
-    path('dashboardHome', views.dashboardHome, name="dashboardHome"),
+    #path('dashboardHome', views.dashboardHome, name="dashboardHome"),
+    path('dashboardHome', views.dashboardHome.as_view(), name="dashboardHome"),
     path('preferences', views.preferences, name="preferences"),
     path('generator', views.generator, name="generator"),
     path('create_playlist', views.PlayListCreateView.as_view(), name='create_playlist'),
     path('UserPreferences', views.UserPreferenceView.as_view(), name='UserPreferences'),
-    path('search_results_genre', views.SearchGenre.as_view(), name="search_results_genre"),
-    path('search_results_artist', views.SearchArtist.as_view(), name="search_results_artist"),
-    path('search_results_song', views.SearchSong.as_view(), name="search_results_song"),
-    path('search_results_album', views.SearchAlbum.as_view(), name="search_results_album"),
+    path('search', views.SearchGenre.as_view(), name="search_results_genre"),
+    path('search', views.SearchArtist.as_view(), name="search_results_artist"),
+    path('search', views.SearchSong.as_view(), name="search_results_song"),
+    path('search', views.SearchAlbum.as_view(), name="search_results_album"),
     # path('search', views.SearchGenre.as_view(), name="search_results"),
     path('search_genre_artist', views.SearchArtistsInGenre.as_view(), name="search_genre_artist"),
 ]
