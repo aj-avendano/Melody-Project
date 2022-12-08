ec0fa48e9666da64bf4107eeb4c8b9da16a7c2b4
KuldeepThiara <kuldeepsingh998@gmail.com>
2022-12-08 12:07:22 -0800

    -Converted some URL paths to as class based as_view paths and cleaned up the code.

--- Melody/dash_app/urls.py
+++ Melody/dash_app/urls.py
@@ -7,18 +7,14 @@ app_name = "dash_app"
 
 ##Function Written by Jason Eissayou and Kuldeep and Joaquin
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register"),
+    path("", views.homepage.as_view(), name="homepage"),
+    path("register", views.RegisterRequest.as_view(), name="register"),
     path('login', views.LoginRequest.as_view(), name='login'),
-    path("logout", views.logout_request, name= "logout"),
-    #path('dashboard', views.dashboard, name="dashboard"),
-    #path('dashboardHome', views.dashboardHome, name="dashboardHome"),
+    path('logout', views.LogoutRequest.as_view(), name='logout'),
     path('dashboardHome', views.dashboardHome.as_view(), name="dashboardHome"),
-    path('preferences', views.preferences, name="preferences"),
-    path('generator', views.generator, name="generator"),
+    path('generator', views.generator.as_view(), name="generator"),
     path('add_item', views.PlaylistItemView.as_view(), name='add_item'),
     path('item', views.PlaylistItemsCreateView.as_view(), name='item'),
-
     path('create_playlist', views.PlayListCreateView.as_view(), name='create_playlist'),
     path('display_playlist', views.DisplayPlaylistView.as_view(), name='display_playlist'),
     path('UserPreferences', views.UserPreferenceView.as_view(), name='UserPreferences'),
@@ -26,7 +22,6 @@ urlpatterns = [
     path('search_results_artist', views.SearchArtist.as_view(), name="search_results_artist"),
     path('search_results_song', views.SearchSong.as_view(), name="search_results_song"),
     path('search_results_album', views.SearchAlbum.as_view(), name="search_results_album"),
-    # path('search', views.SearchGenre.as_view(), name="search_results"),
     path('search_genre_artist', views.SearchArtistsInGenre.as_view(), name="search_genre_artist"),
 ]
 
